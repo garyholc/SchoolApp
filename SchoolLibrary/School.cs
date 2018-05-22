c6ac766ada5d0ec46ac0dbe18cda7d6e881b6288
GARY H <garyholc1@hotmail.com>
2018-05-22 21:02:20 +0100

    Add project files.

---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
+        string _twitterAddress;
+        string TwitterAddress
+        {
+            // make sure the twitter address starts with a @ symbol
+            get { return _twitterAddress; }
+            set
+            {
+                if(value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("The Twitter address must begin with @");
+                }
+            }
+        }
+    }
+}
