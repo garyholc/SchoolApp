7eda37e2d84df9b930a46bfb18dde08ac8c6a0f2
GARY H <garyholc1@hotmail.com>
2018-05-22 22:24:39 +0100

    added Schoolforms app

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -55,5 +55,22 @@ namespace SchoolLibrary
             var result = (a + b + c) / 3;
             return result;
         }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine(Address);
+            sb.AppendLine(City);
+            sb.Append(", ");
+            sb.Append(State);
+            sb.Append("  ");
+            sb.Append(Zip);
+
+            return sb.ToString();
+        }
+
+
+
     }
 }
